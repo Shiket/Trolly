9c20ad1c825d28b9ba15bc2c8a983048f1687d46
Shiket <shiket95@gmail.com>
2019-11-11 20:20:10 +0100

    new bg texture

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { SignInScreen, SignUpScreen } from "./src/screens";
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, ImageBackground } from "react-native";
 import styles from "./src/styles/AppStyles";
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <SignInScreen />
+        <ImageBackground source={require('./assets/bg.jpg')} style={{
+          flex: 1,
+          width: null,
+          height: null,
+          resizeMode: 'center'
+        }}>
+          <SignInScreen />
+        </ImageBackground>
       </View>
     );
   }
