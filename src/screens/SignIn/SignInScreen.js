4c2332b01b479e0fd687f2b3ef9a5ef150a4674f
Shiket <shiket95@gmail.com>
2019-11-12 19:01:42 +0100

    Sign in visual update

--- src/screens/SignIn/SignInScreen.js
+++ src/screens/SignIn/SignInScreen.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import styles from "../../styles/AppStyles";
 import SignInForm from "../../components/SignInForm/SignInForm";
 
 export const SignInScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Trolly</Text>
+
+      <View style={styles.logoWrapper}>
+        <Image source={require("../../../assets/wtfLogo5.png")} style={styles.logoImg} />
+      </View>
+
       <SignInForm />
     </View>
   )
--- src/styles/AppStyles.js
+++ src/styles/AppStyles.js
@@ -7,56 +7,67 @@ export default StyleSheet.create({
     justifyContent: "center",
     resizeMode: 'cover'
   },
-  buttonText: {
-    fontSize: 20,
-    textAlign: "center",
-    color: "#000"
-  },
   title: {
     fontSize: 60,
     fontWeight: "bold",
-    color: "#ffffffe5",
-    alignSelf:'center',
-    marginBottom: 30
+    color: "#fffffff5",
+    marginTop: 5,
+    marginBottom: 50
+  },
+  logoImg: {
+    width: 155,
+    height: 155,
+    marginRight: 15,
+    marginBottom: 40,
+  },
+  logoWrapper: {
+    flexDirection: 'column',
+
+    display:'flex',
+    alignItems:'center'
   },
   formInput: {
-    height: 42,
-    margin:20,
+    height: 44,
+    margin: 15,
     marginBottom: 15,
     padding: 10,
-    fontSize:16,
+    fontSize: 16,
     color: "#fff",
-    backgroundColor: "#48CFAFb2",
+    backgroundColor: "#aaaaaa88",
     borderRadius: 10,
+    borderWidth: 2.5,
+    borderColor: '#fff',
   },
   button: {
     fontSize: 22,
-    paddingTop:2,
+    paddingTop: 3,
     textAlign: "center",
-    color: "#48CFAF",
-    width:120,
-    height:35,
+    color: "#76FEC5",
+    overflow: 'hidden',
+    width: 120,
+    height: 40,
     borderRadius: 10,
-    borderWidth: 2,
-    borderColor: '#48CFAF'
+    borderWidth: 2.5,
+    borderColor: '#fff'
   },
-  buttonsWrapper:{
-    marginTop: 20,
-    flexDirection:'row',
+  buttonsWrapper: {
+    marginTop: 25,
+    flexDirection: 'row',
     alignSelf: 'center',
-    marginBottom: 30
+    marginBottom: 20
   },
   link: {
-    color: "#48CFAF",
+    color: "#76FEC5",
     fontSize: 15,
     alignSelf: 'center',
     marginTop: 10,
   },
-    backgroundImage: {
-      flex: 1,
-      width:'100%',
-      height: '100%',
-      resizeMode: 'stretch', // or 'stretch'
-    }
-  }
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+
+}
 );
