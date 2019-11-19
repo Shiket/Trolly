eba05610c4a4689418a78cb2bd737284510ce73e
pankeny <konrad_malocha@hotmail.com>
2019-11-19 21:49:01 +0100

    discard header in stack navigator

--- App.js
+++ App.js
@@ -2,9 +2,14 @@ import { SignInScreen, SignUpScreen } from "./src/screens";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-const NavigationStack = createStackNavigator({
-  Home: SignInScreen,
-  Landing: SignUpScreen
-});
+const NavigationStack = createStackNavigator(
+  {
+    Home: SignInScreen,
+    Landing: SignUpScreen
+  },
+  {
+    headerMode: "none"
+  }
+);
 
 export default createAppContainer(NavigationStack);
