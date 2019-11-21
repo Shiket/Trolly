a4712d89997f6a0a226d63e56b183e58dc115955
pankeny <konrad_malocha@hotmail.com>
2019-11-21 21:55:21 +0100

    trl-003 build backbone for firebase in context

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 import Navigator from "./src/components/navigation/Navigator";
+import Firebase, { FirebaseProvider } from "./src/firebase";
+
 const App = () => {
-  return <Navigator />;
+  return (
+    <FirebaseProvider value={Firebase}>
+      <Navigator />
+    </FirebaseProvider>
+  );
 };
 
 export default App;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,21 @@
 import * as firebase from "firebase";
-import config from "./config";
+import config from "./Config";
 
 firebase.initializeApp(config);
 
-const Firebase = {};
+const Firebase = {
+  loginWithEmail: (email, password) => {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  },
+  signupWithEmail: (email, password) => {
+    return firebase.auth().createUserWithEmailAndPassword(email, password);
+  },
+  signOut: () => {
+    return firebase.auth().signOut();
+  },
+  checkUserAuth: user => {
+    return firebase.auth().onAuthStateChanged(user);
+  }
+};
+
+export default Firebase;
